fe9c75375f00cdfc61c8126fe84bb23232a62dd9
Altaf Quadri <altafmquadri@gmail.com>
2021-03-18 16:55:30 -0400

    first commit

--- python_sandbox_starter/strings.py
+++ python_sandbox_starter/strings.py
@@ -1,6 +1,26 @@
 # Strings in python are surrounded by either single or double quotation marks. Let's look at string formatting and some string methods
 
+name = 'Altaf'
+age = 37
+
+#Concatenate
+
+# print("Hello, my name is " + name + "and I am " + str(age))
 
 # String Formatting
 
+#Arguments by position
+
+# print('My name is {name} and I am {age}'.format(name=name, age=age))
+
+#F-Strings
+
+# print(f'Hellooo, my name is {name} and I am {age}')
+
 # String Methods
+s = 'hello world'
+
+#Capitalize string
+print(s.capitalize())
+
+#many other string methods https://www.w3schools.com/python/python_ref_string.asp
--- python_sandbox_starter/variables.py
+++ python_sandbox_starter/variables.py
@@ -13,3 +13,18 @@ VARIABLE RULES:
   - Must start with a letter or an underscore
   - Can have numbers but can not start with one
 """
+
+# x = 1           #int
+# y = 2.5         #float
+# name = 'Altaf'  #str
+# is_cool = True  #bool
+
+# Multiple assignment
+x, y, name, is_cool = (1, 2.5, 'Altaf', True)
+
+print(x, y, name, is_cool)
+print(type(x))
+
+#Casting
+x = str(x)
+print(type(x))
